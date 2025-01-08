45cfa300e3a66a82274fc6c4cdec399695e49dbc
vasilii iatiuc <72764693+mad-zephyr@users.noreply.github.com>
2025-01-08 11:36:25 +0200

    add fresh front end

---
+++ main_frontend/src/app/layout.tsx
@@ -0,0 +1,7 @@
+import '@/assets/styles/globals.sass';
+
+import { PropsWithChildren } from 'react';
+
+export default async function RootLayout({ children }: PropsWithChildren) {
+  return children;
+}
