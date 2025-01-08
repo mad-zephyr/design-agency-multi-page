840299c95f654e201f32be0e80df99a99be25be3
vasilii iatiuc <72764693+mad-zephyr@users.noreply.github.com>
2025-01-09 00:50:55 +0200

    update strapi

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -1,8 +1,8 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  proxy: true,
   port: env.int("PORT", 1337),
-  url: env("API_CLIENT_URL"),
+  url: env("API_CLIENT_URL", "/"),
+  proxy: true,
   app: {
     keys: env.array("APP_KEYS"),
   },
