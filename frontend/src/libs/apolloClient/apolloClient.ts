3f45b6c56a4838197cd31c05d41bb3685ab63667
vasilii iatiuc <72764693+mad-zephyr@users.noreply.github.com>
2025-01-10 10:37:06 +0200

    frontend: update graphql client

--- frontend/src/libs/apolloClient/apolloClient.ts
+++ frontend/src/libs/apolloClient/apolloClient.ts
@@ -13,6 +13,21 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   const inMemoryConfig: InMemoryCacheConfig = {};
   const cache = new InMemoryCache(inMemoryConfig);
 
+  const fethCache: RequestCache = 'reload';
+
+  const link = new HttpLink({
+    // this needs to be an absolute url, as relative urls cannot be used in SSR
+    uri: `${basePath}/graphql`,
+    headers: {
+      'Cache-Control': 'no-cache',
+    },
+    fetchOptions: { cache: fethCache },
+
+    // you can disable result caching here if you want to
+    // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
+    // fetchOptions: { cache: "no-store" },
+  });
+
   return new ApolloClient({
     cache: cache,
     defaultOptions: {
@@ -20,17 +35,6 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
         fetchPolicy: 'cache-and-network',
       },
     },
-    link: new HttpLink({
-      // this needs to be an absolute url, as relative urls cannot be used in SSR
-      uri: `${basePath}/graphql`,
-      headers: {
-        'Cache-Control': 'no-cache',
-      },
-      fetchOptions: { cache: 'no-cache' },
-
-      // you can disable result caching here if you want to
-      // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-      // fetchOptions: { cache: "no-store" },
-    }),
+    link,
   });
 });
