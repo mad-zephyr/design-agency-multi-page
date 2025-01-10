74b2ed090cbd426d9c9b7366df0cb56d30a30ff3
vasilii iatiuc <72764693+mad-zephyr@users.noreply.github.com>
2025-01-10 11:29:48 +0200

    update apollo config

--- frontend/src/libs/apolloClient/apolloClient.ts
+++ frontend/src/libs/apolloClient/apolloClient.ts
@@ -10,15 +10,13 @@ import { getBasePath } from '../helpers';
 export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   const basePath = getBasePath();
 
-  const cache = new InMemoryCache();
-
   const link = new HttpLink({
     uri: `${basePath}/graphql`,
     fetchOptions: { cache: 'no-store' },
   });
 
   return new ApolloClient({
-    cache: cache,
+    cache: new InMemoryCache(),
     link,
   });
 });
