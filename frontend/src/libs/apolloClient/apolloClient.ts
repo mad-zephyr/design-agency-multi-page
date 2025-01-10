b6a1aa50d64a4d22512240ec43ad2e3cdbf8979b
vasilii iatiuc <72764693+mad-zephyr@users.noreply.github.com>
2025-01-10 10:08:51 +0200

    update graphql in frontend

--- frontend/src/libs/apolloClient/apolloClient.ts
+++ frontend/src/libs/apolloClient/apolloClient.ts
@@ -1,4 +1,4 @@
-import { HttpLink } from '@apollo/client';
+import { HttpLink, InMemoryCacheConfig } from '@apollo/client';
 import {
   ApolloClient,
   InMemoryCache,
@@ -10,11 +10,24 @@ import { getBasePath } from '../helpers';
 export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   const basePath = getBasePath();
 
+  const inMemoryConfig: InMemoryCacheConfig = {};
+  const cache = new InMemoryCache(inMemoryConfig);
+
   return new ApolloClient({
-    cache: new InMemoryCache(),
+    cache: cache,
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: 'cache-and-network',
+      },
+    },
     link: new HttpLink({
       // this needs to be an absolute url, as relative urls cannot be used in SSR
       uri: `${basePath}/graphql`,
+      headers: {
+        'Cache-Control': 'no-cache',
+      },
+      fetchOptions: { cache: 'no-cache' },
+
       // you can disable result caching here if you want to
       // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
       // fetchOptions: { cache: "no-store" },
